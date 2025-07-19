8949bdd86c9626153d5d3b0e5715d64c9be816ca
sandunudayakanthaproducts <sndnudayakantha@gmail.com>
2025-07-19 19:18:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,7 @@ if (!cached) {
 async function connectToDatabase() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(process.env.MONGODB_URI);
   }
   cached.conn = await cached.promise;
   return cached.conn;
